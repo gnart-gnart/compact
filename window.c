4272d9b79e21c8d2271384a7dd3d0ab091afb2db
grant <grantwoxford@gmail.com>
2026-07-14 19:55:14 -0500

    most basic window wrapper

--- window.c
+++ window.c
@@ -1,1 +1,12 @@
 #include "window.h"
+
+#include <raylib.h>
+
+void startWindow() {
+    InitWindow(windowWidth, windowHeight, "gnart's game");
+    SetTargetFPS(60);
+}
+
+void stopWindow() {
+    CloseWindow();
+}
